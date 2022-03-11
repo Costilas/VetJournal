2c33f6ff0de79b3339d1748f5d2488865f87f222
Sergey Kuzmin <winterfield91@yandex.ru>
2022-03-11 17:16:23 +0300

    Installing dev bar

---
+++ app/resources/views/layouts/layout.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/app.css">
+        <title>PetVetZooMed</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div class="container">
+                    <div class="header_content">
+                        <div class="logo header-item">
+                            1
+                        </div>
+                        <ul class="nav header-item menu">
+                            <li class="nav-item">Главная</li>
+                            <li class="nav-item">Заметки</li>
+                            <li class="nav-item">Приемы</li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div class="container">
+                    1
+                </div>
+            </main>
+            <footer>
+                <div class="container">
+                    <div class="footer_content">
+                        <div class="footer_item">1</div>
+                        <div class="footer_item">2</div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    <script src="js/app.js"></script>
+    </body>
+</html>
